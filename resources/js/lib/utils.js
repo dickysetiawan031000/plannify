07a171ecc7ebabaca86b588322305ef18327660e
dickysetiawan031000 <dickysetiawan031000@gmail.com>
2025-01-14 22:07:31 +0700

    Configuration Setup

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,6 +1,22 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
+
+export const STATUS = {
+    TODO: 'To Do',
+    INPROGRESS: 'In Progress',
+    ONREVIEW: 'On Review',
+    DONE: 'Done',
+    UNKNOWN: 'Unknown',
+};
+
+export const PRIORITY = {
+    LOW: 'Low',
+    MEDIUM: 'Medium',
+    HIGH: 'High',
+    URGENT: 'Urgent',
+    UNKNOWN: 'Unknown',
+};
