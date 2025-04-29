681d868f9b72747a91a64d65450191541be385cb
Dicky Setiawan <dickysetiawan031000@gmail.com>
2025-04-29 15:09:11 +0700

    add tasks table and Task model for task management

---
+++ app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tasks';
+    protected $fillable = [
+        'card_id',
+        'user_id',
+        'parent_id',
+        'title',
+        'is_completed',
+    ];
+}
