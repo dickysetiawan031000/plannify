899a99892e0831c124b1f523499044805589cf1b
Dicky Setiawan <dickysetiawan031000@gmail.com>
2025-05-13 15:51:01 +0700

    add Sidebar and SidebarResponsive components for improved navigation

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -3,6 +3,8 @@ import { Head, Link, usePage } from '@inertiajs/react';
 import { Fragment, useState } from 'react';
 import { PiHouse, PiLockKeyOpen, PiPlus, PiSidebar, PiSquaresFour, PiUser, PiX } from 'react-icons/pi';
 import { Avatar, AvatarFallback } from '@/Components/ui/avatar';
+import Sidebar from "@/Layouts/Partials/Sidebar.jsx";
+import SidebarResponsive from "@/Layouts/Partials/SidebarResponsive.jsx";
 
 export default function AppLayout({ children, title }) {
     const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -56,88 +58,8 @@ export default function AppLayout({ children, title }) {
                                             </button>
                                         </div>
                                     </Transition.Child>
-                                    <div className="flex flex-col px-6 pb-2 overflow-y-auto bg-white dark:bg-gray-900 grow gap-y-5">
-                                        <div className="flex h-16 shrink-0 items-center space-x-1.5">
-                                            <Link
-                                                href="/"
-                                                className="-m-1.5 p-1.5 text-2xl font-black leading-relaxed tracking-tighter"
-                                            >
-                                                Plannify<span className="text-red-500">.</span>
-                                            </Link>
-                                        </div>
-                                        <nav className="flex flex-col flex-1">
-                                            <ul role="list" className="flex flex-col flex-1 gap-y-7">
-                                                <li>
-                                                    <ul role="list" className="-mx-2 space-y-1">
-                                                        {/* menu */}
-                                                        <li>
-                                                            <Link
-                                                                href={'#'}
-                                                                className={'flex p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
-                                                            >
-                                                                <PiHouse className={'w-6 h-6 shrink-0 text-foreground'} />
-                                                                Dashboard
-
-                                                            </Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link
-                                                                href={'#'}
-                                                                className={'flex p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
-                                                            >
-                                                                <PiUser className={'w-6 h-6 shrink-0 text-foreground'} />
-                                                                People
-
-                                                            </Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link
-                                                                href={'#'}
-                                                                className={'flex p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
-                                                            >
-                                                                <PiSquaresFour className={'w-6 h-6 shrink-0 text-foreground'} />
-                                                                My Task
-
-                                                            </Link>
-                                                        </li>
-                                                        <li>
-                                                            <Link
-                                                                href={'#'}
-                                                                className={'flex p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
-                                                            >
-                                                                <PiLockKeyOpen className={'w-6 h-6 shrink-0 text-foreground'} />
-                                                                Logout
-
-                                                            </Link>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                                <li>
-                                                    {/* workspaces */}
-                                                    <div className={'flex items-center justify-between'}>
-                                                        <div className={'text-xs font-semibold leading-relaxed uppercase text-foreground'}>
-                                                            Workspaces
-                                                        </div>
-                                                        <Link>
-                                                            <PiPlus className={'w-4 h-4 text-foreground hover:text-red-500'} />
-                                                        </Link>
-                                                    </div>
-                                                    <ul role={'list'} className={'mt-2 -mx-2 space-y-1'}>
-                                                        <li>
-                                                            <Link
-                                                                href={'#'}
-                                                                className={'flex w-full p-3 text-sm font-semibold leading-relaxed rounded-md group gap-x-3 text-foreground hover:bg-gray-100'}
-                                                            >
-                                                                <span className={'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium border-foreground text-foreground'}>B</span>
-                                                                <span className={'truncate'}>Backend Developer</span>
-                                                            </Link>
-                                                        </li>
-
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </nav>
-                                    </div>
+                                    {/*  Sidebar Responsive  */}
+                                    <SidebarResponsive/>
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
@@ -155,86 +77,7 @@ export default function AppLayout({ children, title }) {
                             </Link>
                         </div>
                         {/* Sidebar */}
-                        <nav className="flex flex-col flex-1">
-                            <ul role="list" className="flex flex-col flex-1 gap-y-7">
-                                <li>
-                                    <ul role="list" className="-mx-2 space-y-2">
-                                        {/* menu */}
-                                        <li>
-                                            <Link
-                                                href={'#'}
-                                                className={'flex p-3 text-sm font-semibold leading-relaxed tracking-tighter rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
-                                            >
-                                                <PiHouse className={'w-6 h-6 text-foreground shrink-0'} />
-                                                Dashboard
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                href={'#'}
-                                                className={'flex p-3 text-sm font-semibold leading-relaxed tracking-tighter rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
-                                            >
-                                                <PiUser className={'w-6 h-6 text-foreground shrink-0'} />
-                                                People
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                href={'#'}
-                                                className={'flex p-3 text-sm font-semibold leading-relaxed tracking-tighter rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
-                                            >
-                                                <PiSquaresFour className={'w-6 h-6 text-foreground shrink-0'} />
-                                                My Tasks
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                href={'#'}
-                                                className={'flex p-3 text-sm font-semibold leading-relaxed tracking-tighter rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
-                                            >
-                                                <PiLockKeyOpen className={'w-6 h-6 text-foreground shrink-0'} />
-                                                Logout
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    {/* workspaces */}
-                                    <div className={'flex items-center justify-between'}>
-                                        <div className={'text-xs font-semibold leading-relaxed uppercase text-foreground'}>
-                                            Workspaces
-                                        </div>
-                                        <Link>
-                                            <PiPlus className={'w-4 h-4 text-foreground hover:text-red-500'} />
-                                        </Link>
-                                    </div>
-                                    <ul role={'List'} className={'mt-2 -mx-2 space-y-2'}>
-                                        <li>
-                                            <Link
-                                                href={'#'}
-                                                className={'flex w-full p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
-                                            >
-                                                <span className={'border-foreground text-foreground flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium'}>B</span>
-                                                <span>Backend Developer</span>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li className="mt-auto -mx-6">
-                                    {/* profile */}
-                                    <Link
-                                        href={'#'}
-                                        className={'flex items-center px-6 py-3 text-sm font-semibold leading-relaxed gap-x-4 text-foreground hover:bg-gray-100'}
-                                    >
-                                        <Avatar>
-                                            <AvatarFallback>X</AvatarFallback>
-                                        </Avatar>
-                                        <span>Dicky Setiawan</span>
-
-                                    </Link>
-                                </li>
-                            </ul>
-                        </nav>
+                        <Sidebar />
                     </div>
                 </div>
 
---
+++ resources/js/Layouts/Partials/SidebarResponsive.jsx
@@ -0,0 +1,89 @@
+import { PiHouse, PiLockKeyOpen, PiPlus, PiSidebar, PiSquaresFour, PiUser, PiX } from 'react-icons/pi';
+import { Link } from "@inertiajs/react";
+
+export default function SidebarResponsive() {
+    return (
+        <div className="flex flex-col px-6 pb-2 overflow-y-auto bg-white dark:bg-gray-900 grow gap-y-5">
+            <div className="flex h-16 shrink-0 items-center space-x-1.5">
+                <Link
+                    href="/"
+                    className="-m-1.5 p-1.5 text-2xl font-black leading-relaxed tracking-tighter"
+                >
+                    Plannify<span className="text-red-500">.</span>
+                </Link>
+            </div>
+            <nav className="flex flex-col flex-1">
+                <ul role="list" className="flex flex-col flex-1 gap-y-7">
+                    <li>
+                        <ul role="list" className="-mx-2 space-y-1">
+                            {/* menu */}
+                            <li>
+                                <Link
+                                    href={'#'}
+                                    className={'flex p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
+                                >
+                                    <PiHouse className={'w-6 h-6 shrink-0 text-foreground'} />
+                                    Dashboard
+
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href={'#'}
+                                    className={'flex p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
+                                >
+                                    <PiUser className={'w-6 h-6 shrink-0 text-foreground'} />
+                                    People
+
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href={'#'}
+                                    className={'flex p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
+                                >
+                                    <PiSquaresFour className={'w-6 h-6 shrink-0 text-foreground'} />
+                                    My Task
+
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href={'#'}
+                                    className={'flex p-3 text-sm font-semibold leading-relaxed rounded-md text-foreground hover:bg-gray-100 group gap-x-3'}
+                                >
+                                    <PiLockKeyOpen className={'w-6 h-6 shrink-0 text-foreground'} />
+                                    Logout
+
+                                </Link>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        {/* workspaces */}
+                        <div className={'flex items-center justify-between'}>
+                            <div className={'text-xs font-semibold leading-relaxed uppercase text-foreground'}>
+                                Workspaces
+                            </div>
+                            <Link>
+                                <PiPlus className={'w-4 h-4 text-foreground hover:text-red-500'} />
+                            </Link>
+                        </div>
+                        <ul role={'list'} className={'mt-2 -mx-2 space-y-1'}>
+                            <li>
+                                <Link
+                                    href={'#'}
+                                    className={'flex w-full p-3 text-sm font-semibold leading-relaxed rounded-md group gap-x-3 text-foreground hover:bg-gray-100'}
+                                >
+                                    <span className={'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium border-foreground text-foreground'}>B</span>
+                                    <span className={'truncate'}>Backend Developer</span>
+                                </Link>
+                            </li>
+
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
