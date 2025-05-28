4615504ae5a5bbf1a469fdb948c0a1fa57dbc6e4
Dicky Setiawan <dickysetiawan031000@gmail.com>
2025-05-28 16:22:21 +0700

    Update grid layout to use three columns on medium screens

--- resources/js/Pages/Workspaces/Create.jsx
+++ resources/js/Pages/Workspaces/Create.jsx
@@ -40,7 +40,7 @@ export default function Create({page_settings, visibilities}) {
 
     return (
         <div className={'space-y-10 divide-y divide-dashed divide-gray-900/10'}>
-            <div className={'grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-8'}>
+            <div className={'grid grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-8'}>
                 <HeaderForm className={'mb-8'} title={page_settings.title} subtitle={page_settings.subtitle} />
 
                 <Card className={'md:col-span-2'}>
